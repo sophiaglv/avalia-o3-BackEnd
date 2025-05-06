a133bacd1a62f9791b95ce5bd47bec277129fb26
Sophia Galvão <159277699+sophiaglv@users.noreply.github.com>
2025-05-06 12:18:00 -0300

    Delete GerenciamentoDeCidades directory

--- GerenciamentoDeCidades/src/main/java/com/cidade/repository/CidadeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cidade.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cidade.entities.Cidade;
-
-public interface CidadeRepository extends JpaRepository<Cidade, Long>{
-
-}
